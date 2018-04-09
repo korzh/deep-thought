1a85bcfb328dc617711e9e55d8f1976d4dc9c709
Sergiy Korzh <sergey.korzh@live.com>
2018-04-09 18:42:46 +0300

    Datasets. Initial argument processing setup

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Korzh.DeepThought
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) {
+                Console.WriteLine("Using: deepth <command> <KB_ID> <XML file> [other options]");
+                return;
+            }
+
+            var command = args[0];
+            var kbId = args[1];
+            var xmlFileName = args[2];
+
+            if (command == "train") {
+            }
+            else if (command == "test") {
+
+            }
+
+        }
+    }
+}
